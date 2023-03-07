db27bc9a563b9fea557a0a6f20c3f4d70001b77b
Danilo Souza <dev.danilosouza@gmail.com>
2023-03-07 20:47:52 -0300

    HelloWorld em C#

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tt.Models;
+
+namespace tt.Controllers;
+
+public class HelloWorldController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HelloWorldController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error");
+    }
+}
