c9ce0dfbb0149abc803b66d1d017a5778d47058f
Danilo Souza <dev.danilosouza@gmail.com>
2023-03-07 20:54:00 -0300

    HelloWorld em C#

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -18,11 +18,6 @@ public class HelloWorldController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
